dd95a4c51bd370861ecd2d5f06f6f7fbe3014f69
ananyatewari <ananyatewari0205@gmail.com>
2023-11-11 02:59:42 +0530

    done

--- score.js
+++ score.js
@@ -1,1 +1,11 @@
 // Iteration 8: Making scoreboard functional
+var finalScore = document.getElementById("score-board");
+const playAgain = document.getElementById("play-again-button");
+
+var score = localStorage.getItem("score");
+
+finalScore.innerHTML = score;
+
+playAgain.addEventListener ("click", () => {
+    window.location.href = "./game.html"
+}) //done :D
